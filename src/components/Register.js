aaab3a2fb44a09bf09eaf7a58cd301f15945ea53
thinleyd <thinley.dema@thimphutechpark.bt>
2022-10-21 13:58:01 +0600

    changed

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,31 @@
 import React, { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { Link  } from 'react-router-dom';
+import regpic from "../images/regpic.png";
 import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import { isEmail } from "validator";
-
+import TextField from '@material-ui/core/TextField';
+import Grid from "@material-ui/core/Grid";
+//import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+//import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { register } from "../actions/auth";
+
+
+// const useStyles = makeStyles((theme) => ({
+//   formControl: {
+//     margin: theme.spacing(1),
+//     minWidth: 120,
+//   },
+//   selectEmpty: {
+//     marginTop: theme.spacing(2),
+//   },
+// }));
+
 
 const required = (value) => {
   if (!value) {
@@ -28,11 +47,11 @@ const validEmail = (value) => {
   }
 };
 
-const vusername = (value) => {
+const vfullname = (value) => {
   if (value.length < 3 || value.length > 20) {
     return (
       <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
+        The fullname must be between 3 and 20 characters.
       </div>
     );
   }
@@ -48,11 +67,15 @@ const vpassword = (value) => {
   }
 };
 
+
 const Register = () => {
   const form = useRef();
   const checkBtn = useRef();
 
-  const [username, setUsername] = useState("");
+  const [username, setFullname] = useState("");
+  const [gender, setGender] = React.useState('');
+
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [successful, setSuccessful] = useState(false);
@@ -60,9 +83,12 @@ const Register = () => {
   const { message } = useSelector(state => state.message);
   const dispatch = useDispatch();
 
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
+  const onChangeFullname = (e) => {
+    const fullname = e.target.value;
+    setFullname(fullname);
   };
 
   const onChangeEmail = (e) => {
@@ -92,12 +118,14 @@ const Register = () => {
         });
     }
   };
-
+  // const classes = useStyles();
   return (
+    
+
     <div className="col-md-12">
-      <div className="card card-container">
+      <div className="regcard regcard-container">
         <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          src={regpic}
           alt="profile-img"
           className="profile-img-card"
         />
@@ -106,7 +134,167 @@ const Register = () => {
           {!successful && (
             <div>
               <div className="form-group">
-                <label htmlFor="username">Username</label>
+              <Grid container spacing={2}>
+          
+            <Grid item xs={12} sm={12}>
+              <TextField
+                autoComplete="fname"
+                type="text"
+                name="fName"
+                variant="outlined"
+                required
+                fullWidth
+                id="fName"
+                onChange={onChangeFullname}
+                validations={[required, vfullname]}
+                label="Full Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="cid"
+                type="number"
+                variant="outlined"
+                required
+                fullWidth
+                id="cid"
+                label="CID"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+               <FormControl variant="outlined" className="formControl">
+               <InputLabel id="demo-simple-select-outlined-label">Gender</InputLabel>
+                 <Select
+                     labelId="demo-simple-select-outlined-label"
+                     id="demo-simple-select-outlined"
+                     value={gender}
+                     onChange={handleChange}
+                     label="Gender"
+                 >
+                   <MenuItem value={10}>Female</MenuItem>
+                   <MenuItem value={20}>Male</MenuItem>
+         
+                  </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="dzongkhag"
+                type="text"
+                name="dzongkhag"
+                variant="outlined"
+                required
+                fullWidth
+                id="dzongkhag"
+                label="Dzongkhag"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                type="text"
+                required
+                fullWidth
+                id="gewog"
+                label="Gewog"
+                name="gewog"
+                autoComplete="gewog"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="village"
+                type="text"
+                name="village"
+                variant="outlined"
+                required
+                fullWidth
+                id="village"
+                label="Village"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                type="text"
+                required
+                fullWidth
+                id="current address"
+                label="Current Address"
+                name="current address"
+                autoComplete="current address"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="dob"
+                type="date"
+                name="dob"
+                variant="outlined"
+                required
+                fullWidth
+                id="date"
+                
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                type="number"
+                required
+                fullWidth
+                id="Mno"
+                label="Mobile No."
+                name="Mno"
+                autoComplete="Mno"
+              />
+            </Grid>  
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                className="width"
+                autoComplete="email"
+                onChange={onChangeEmail}
+                validations={[required, validEmail]}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={onChangePassword}
+                validations={[required, vpassword]}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Confirm Password"
+                type="password"
+                id="Cpassword"
+                autoComplete="Confirm-password"
+                onChange={onChangePassword}
+                validations={[required, vpassword]}
+              />
+            </Grid>
+            </Grid>
+                {/* <label htmlFor="username">Username</label>
                 <Input
                   type="text"
                   className="form-control"
@@ -114,10 +302,10 @@ const Register = () => {
                   value={username}
                   onChange={onChangeUsername}
                   validations={[required, vusername]}
-                />
+                /> */}
               </div>
 
-              <div className="form-group">
+              {/* <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <Input
                   type="text"
@@ -127,9 +315,9 @@ const Register = () => {
                   onChange={onChangeEmail}
                   validations={[required, validEmail]}
                 />
-              </div>
+              </div> */}
 
-              <div className="form-group">
+              {/* <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <Input
                   type="password"
@@ -139,11 +327,14 @@ const Register = () => {
                   onChange={onChangePassword}
                   validations={[required, vpassword]}
                 />
-              </div>
+              </div> */}
 
               <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
+                <button className="btn">Sign Up</button>
               </div>
+              <p className="link">
+              <a href="#">Already have account</a> or <Link to="/login">Sign in</Link>
+             </p>
             </div>
           )}
 
